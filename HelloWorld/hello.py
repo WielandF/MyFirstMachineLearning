cb096d41c138fb9db1bac896018ef5f73e032781
WielandF <fabian-wieland@gmx.de>
2018-08-01 15:42:59 +0200

    Implements Models for prediction

--- HelloWorld/hello.py
+++ HelloWorld/hello.py
@@ -33,7 +33,8 @@ from sklearn.svm import SVC
 
 
  #Load dataset
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+#url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+url = "C:/Users/Fabian Wieland/Documents/GitHub/MyFirstMachineLearning/HelloWorld/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
 
@@ -54,3 +55,39 @@ seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
 scoring = 'accuracy'
+seed=7
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+
+  # Compare Algorithms
+#fig = plt.figure()
+#fig.suptitle('Algorithm Comparison')
+#ax = fig.add_subplot(111)
+#plt.boxplot(results)
+#ax.set_xticklabels(names)
+#plt.show()  
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
